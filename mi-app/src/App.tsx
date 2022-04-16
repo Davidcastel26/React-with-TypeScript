453b18697c52d136dc9ffffa895e66519603d7c3
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 14:01:02 -0600

    Update App.tsx

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,11 +1,12 @@
 import Counter from "./components/Counter";
+import User from "./components/User";
 
 function App() {
   return (
     <>
       <h1> React + TS!</h1>
       <hr />
-
+      <User />
       <Counter />
     </>
   );
