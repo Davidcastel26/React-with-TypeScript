b1ec4f2d839536856869cc103d6a5e8258b490a3
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 14:00:30 -0600

    Create User.tsx

---
+++ mi-app/src/components/User.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react"
+
+const User = () => {
+
+  const [user, setUser] = useState()
+
+  return (
+    <div>
+      <h3>User: useState</h3>
+
+      <button className="btn btn-outline-primary">
+        
+      </button>
+    </div>
+  )
+}
+
+export default User
