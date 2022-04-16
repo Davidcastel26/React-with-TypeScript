4ca6473a9578b62773dbd84ef3cb558784aa16db
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 16:20:50 -0600

    Update App.tsx

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -4,10 +4,16 @@ import User from "./components/User";
 function App() {
   return (
     <>
-      <h1> React + TS!</h1>
+      <h1 className="mt-4"> React + TS!</h1>
       <hr />
+      
+      <h2> useState</h2>
       <User />
       <Counter />
+
+      <h2>useEffect - useRef</h2>
+      <hr />
+      
     </>
   );
 }
