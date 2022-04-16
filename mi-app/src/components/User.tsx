c2c17284753dfb1ebd4bbb4c87e4edef0a9329cb
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 14:17:49 -0600

    Update User.tsx

--- mi-app/src/components/User.tsx
+++ mi-app/src/components/User.tsx
@@ -1,18 +1,43 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const User = () => {
-
-  const [user, setUser] = useState()
-
-  return (
-    <div>
-      <h3>User: useState</h3>
-
-      <button className="btn btn-outline-primary">
-        
-      </button>
-    </div>
-  )
+interface User {
+  uid: string;
+  name: string;
 }
 
-export default User
+/*
+  // YOU CAN USE A CONST INTO THE USE STATE
+  const tempUser: User = {
+    uid:'XCW345',
+    name: 'asd'
+  }
+*/
+
+const User = () => {
+  const [user, setUser] = useState<User>();
+
+  const login = () => {
+    setUser({
+      uid: "ABC123",
+      name: "Dave",
+    });
+  };
+
+  return (
+    <div className="mb-5">
+      <h3>User: useState</h3>
+
+      <button onClick={login} className="btn btn-outline-primary">
+        Login
+      </button>
+
+      {!user ? (
+        <pre> There is not such user here, it should be in other part tho!</pre>
+      ) : (
+        <pre> {JSON.stringify(user)} </pre>
+      )}
+    </div>
+  );
+};
+
+export default User;
