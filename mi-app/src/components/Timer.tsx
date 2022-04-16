7c3d64c9628781e9bb8d402620305522360d8f71
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 16:46:11 -0600

    update

--- mi-app/src/components/Timer.tsx
+++ mi-app/src/components/Timer.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const Timer = () => {
+const Timer = (millisec:any) => {
 
   const [seconds, setSeconds] = useState(0)
 
--- mi-app/src/components/TimerPadre.tsx
+++ mi-app/src/components/TimerPadre.tsx
@@ -1,15 +1,28 @@
-import Timer from './Timer';
+import { useState } from 'react';
+import Timer from './Timer.tsx';
 
 const TimerPadre = () => {
+
+  const [milliseconds, setMilliseconds] = useState(1000)
+    
   return (
     <>
       <span>Miliseguntods { 1000 } </span>
 
       <br />
-      <button className="btn btn-outline-success">
+      <button 
+        className="btn btn-outline-success"
+        onClick={() => setMilliseconds(1000)}
+      >
         1000
       </button>
-      <Timer />
+      <button 
+        className="btn btn-outline-success"
+        onClick={() => setMilliseconds(2000)}  
+      >
+        2000
+      </button>
+      <Timer millisec={milliseconds}/>
     </>
   )
 }
