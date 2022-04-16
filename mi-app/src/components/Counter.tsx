324be5830ee08592a883d2d59c8b369dc38b51a1
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-04-16 13:57:19 -0600

    Update Counter.tsx

--- mi-app/src/components/Counter.tsx
+++ mi-app/src/components/Counter.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react"
 
 const Counter = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = ( number: number = 1 ):void => {
+    setCounter( counter + number)
+  }
+
   return (
     <div className="mat-5">
-      <h3>Counter: useState</h3>      
+      <h3>Counter: useState</h3>  
+
+      <span> Valor: { counter } </span>  
+      <br /> 
+      <button 
+        className="btn btn-outline-primary mt-2"
+        onClick={() => increment()}
+      >
+        +1 
+      </button> 
+
+      <button 
+        className="btn btn-outline-primary mt-2"
+        onClick={() => increment(2)}
+      >
+        +2
+      </button> 
+      
+      <button 
+        className="btn btn-outline-danger mt-2"
+        onClick={() => setCounter(0)}
+      >
+        Reset
+      </button> 
+
     </div>
   )
 }
